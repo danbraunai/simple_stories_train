a63ae7145a968b2bef0413672b0b339aa4527e2e
Thomas Marshall <>
2024-08-08 10:54:00 -0400

    Set type annotations for args and kwargs to Any, hopefully to satisfy pyright

--- simple_stories_train/utils.py
+++ simple_stories_train/utils.py
@@ -5,7 +5,7 @@ import torch
 from torch import nn
 from typing import Any
 
-def print0(*args: tuple[Any], **kwargs: dict[str, Any]):
+def print0(*args: Any, **kwargs: Any):
     # modified print that only prints from the master process
     # if this is not a distributed run, it's just a print
     if int(os.environ.get("RANK", 0)) == 0:
